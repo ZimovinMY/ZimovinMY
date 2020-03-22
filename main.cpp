185928cef34d88cca611bcab8285a1bf88c0b145
Alice (ZimovinMY) <alice@example.com>
2020-03-22 22:29:36 +0300

    Revert "..."
    This reverts commit 0bf0a3bdc40cf8ce28a1cd325ef37db6dc27d7d3.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
-    cout << "A + B = " << a + b << '\n'
-     << "A - B = " << a - b << '\n';
+    cout << "Sum of a and b: " <<a+b<<endl;
+    cout << "Difference a and b: " <<a-b;
     return 0;
 }
